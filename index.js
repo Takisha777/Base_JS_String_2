fbd53ee8a1d15cea1d72cca6d7b37308aa4ba699
Takisha777 <alexey.kovtun.od@gmail.com>
2024-05-19 08:25:14 +0200

    let

---
+++ index.js
@@ -0,0 +1,7 @@
+const myName = 'Alex',
+programmingLanguage = 'JavaScript',
+courseCreatorName = 'Vladilen Minin',
+reasonText = 'это относительная свобода, да и всё время учить что то новое',
+numberOfMonth = 'два';
+
+let myInfoText = `Всем привет! Меня зовут ${myName}. Сейчас я изучаю язык программирования ${programmingLanguage} на курсе по ${programmingLanguage} у ${courseCreatorName}. Я хочу стать веб-разработчиком, потому что ${reasonText}. До этого я изучал ${programmingLanguage} ${numberOfMonth} месяца. Я уверен, что пройду данный курс до конца!`;
